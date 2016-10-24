e6887005a949fc7070670ad88bd913e361d5b04f
Ragnar B. Johannsson <ragnar@igo.is>
2016-10-24 21:29:55 +0000

    Constants

---
+++ constants.go
@@ -0,0 +1,13 @@
+package babble
+
+const (
+	DEFAULT_SENTENCE_TRIES    = 10
+	DEFAULT_MAX_OVERLAP_RATIO = 0.7
+	DEFAULT_MAX_OVERLAP_TOTAL = 15
+	DEFAULT_STATE_SIZE        = 2
+)
+
+const (
+	chainStart = "__BEGIN__"
+	chainEnd   = "__END__"
+)
